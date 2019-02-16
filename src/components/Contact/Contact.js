5bd8abc9d9f4f17e22d453b9fef7162a1dc352a0
Jong Park <maclay13531@gmail.com>
2019-02-16 17:46:53 -0500

    update - contact panel info / update - contact panel responsiveness

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,7 +5,13 @@ import styles from './Contact.module.css';
 const contact = React.forwardRef((props, ref) => {
     return (
         <div className={styles.Contact} ref={ref}>
-
+            <div className={styles.Content}>
+                <div className={styles.GeneralInfo}>
+                    <div className={styles.GeneralInfoLabel}>Contact Info</div>
+                    <p>850.339.4834</p>
+                    <p>maclay13531@gmail.com</p>
+                </div>
+            </div>
         </div>
     );
 });
