9f6c43872ae324151daa59d1dfa28d324ce4ce54
Jong Park <maclay13531@gmail.com>
2019-02-20 22:42:40 -0500

    update - resume / update - typo on description

--- src/components/AboutMe/Description/MyDescription/MyDescription.js
+++ src/components/AboutMe/Description/MyDescription/MyDescription.js
@@ -6,7 +6,7 @@ import MySkill from './MySkill/MySkill';
 const myDescription = (props) => {
     return (
         <div className={styles.MyDescription}>
-            <p>Hello! My name is Jong park, a web developer & software engineer from Atlanta Georgia.
+            <p>Hello! My name is Jong Park, a web developer & software engineer from Atlanta, Georgia.
             As an engineer, there is nothing more satisfying than being a creator of
             well developed projects, and no matter the size of your projects,
             let me solve your problems!</p>
